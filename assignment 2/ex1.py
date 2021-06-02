f857fc4f7a154ca3204c360dcde31cef254953fa
Sarika Mohanraj <66568393+sarikamohan08@users.noreply.github.com>
2021-06-02 12:05:34 +0530

    Add files via upload

---
+++ assignment 2/ex1.py
@@ -0,0 +1,22 @@
+for i in range(1,6):
+    for j in range(1,i+1):
+        print("*",end=' ')
+    print()
+for k in range(5,1,-1):
+    for l in range(1,k):
+        print("*",end=' ')
+    print()
+
+
+"""
+o/p:
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+* * * * 
+* * * 
+* * 
+*
+"""
---
+++ assignment 2/ex2.py
@@ -0,0 +1,6 @@
+word=input()
+print(word[-1::-1])
+"""o/p:
+sarika
+akiras
+"""
